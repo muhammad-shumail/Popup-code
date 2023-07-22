c7b22e17019ffd22f0a161a7957f52a0c3a1928f
Muhammad Shumail <thinktechnologies94@gmail.com>
2023-07-22 22:03:37 +0500

    box image is working now

--- jquery-cropper.js
+++ jquery-cropper.js
@@ -1,70 +1,82 @@
-/*!
- * jQuery Cropper v1.0.1
- * https://fengyuanchen.github.io/jquery-cropper
- *
- * Copyright 2018-present Chen Fengyuan
- * Released under the MIT license
- *
- * Date: 2019-10-19T08:48:33.062Z
- */
-
+// Use an immediately invoked function expression (IIFE) to avoid polluting the global scope
 (function (global, factory) {
+  // Use a ternary operator to check the module system and call the factory function accordingly
   typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('jquery'), require('cropperjs')) :
     typeof define === 'function' && define.amd ? define(['jquery', 'cropperjs'], factory) :
+      // If no module system, use the global jQuery and Cropper objects
       (global = global || self, factory(global.jQuery, global.Cropper));
 }(this, function ($, Cropper) {
   'use strict';
 
-  $ = $ && $.hasOwnProperty('default') ? $['default'] : $;
-  Cropper = Cropper && Cropper.hasOwnProperty('default') ? Cropper['default'] : Cropper;
+  // Use destructuring assignment to get the default exports of jQuery and Cropper
+  $ = $?.default ?? $;
+  Cropper = Cropper?.default ?? Cropper;
 
+  // Check if jQuery and Cropper are available
   if ($ && $.fn && Cropper) {
+    // Save the original cropper plugin in case of conflict
     var AnotherCropper = $.fn.cropper;
+    // Define a namespace for the plugin
     var NAMESPACE = 'cropper';
 
-    $.fn.cropper = function jQueryCropper(option) {
-      for (var _len = arguments.length, args = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-        args[_key - 1] = arguments[_key];
-      }
-
+    // Define the plugin function
+    $.fn.cropper = function jQueryCropper(option, ...args) {
+      // Initialize the result variable
       var result;
+      // Loop over each element in the jQuery collection
       this.each(function (i, element) {
+        // Get the jQuery object of the element
         var $element = $(element);
+        // Check if the option is 'destroy'
         var isDestroy = option === 'destroy';
+        // Get the cropper instance from the element data
         var cropper = $element.data(NAMESPACE);
 
+        // If no cropper instance, create one or skip if destroying
         if (!cropper) {
           if (isDestroy) {
             return;
           }
 
+          // Merge the default options, the element data and the option object
           var options = $.extend({}, $element.data(), $.isPlainObject(option) && option);
+          // Create a new cropper instance with the element and the options
           cropper = new Cropper(element, options);
+          // Store the cropper instance in the element data
           $element.data(NAMESPACE, cropper);
         }
 
+        // If the option is a string, treat it as a method name
         if (typeof option === 'string') {
+          // Get the method from the cropper instance
           var fn = cropper[option];
 
+          // If the method is a function, call it with the arguments
           if ($.isFunction(fn)) {
+            // Call the method and store the result
             result = fn.apply(cropper, args);
 
+            // If the result is the cropper instance, set it to undefined
             if (result === cropper) {
               result = undefined;
             }
 
+            // If destroying, remove the cropper instance from the element data
             if (isDestroy) {
               $element.removeData(NAMESPACE);
             }
           }
         }
       });
+      // Return the result or the jQuery collection
       return result !== undefined ? result : this;
     };
 
+    // Expose the Cropper constructor and static methods on the plugin function
     $.fn.cropper.Constructor = Cropper;
     $.fn.cropper.setDefaults = Cropper.setDefaults;
 
+    // Define a noConflict method to restore the original cropper plugin
     $.fn.cropper.noConflict = function noConflict() {
       $.fn.cropper = AnotherCropper;
       return this;
--- main.js
+++ main.js
@@ -279,8 +279,80 @@ $(function () {
 });
 
 // Path: main.js
-$(function () {
-  console.log('main.js loaded');
+
+console.log('main.js loaded');
+
+function drawBox(wall_width, wall_height, image_width, image_height) {
+  // image_width = 1024;
+  // image_height = 682;
+  let image_aspect_ratio = image_width / image_height;
+
+  // wall_width = 173;
+  // wall_height = 108;
+  let wall_aspect_ratio = wall_width / wall_height;
+
+  if (wall_aspect_ratio <= image_aspect_ratio) {
+    // subtract width
+    let s = wall_aspect_ratio * image_height - image_width;
+    // wall_width = image_width - s;
+    let start_x = Math.abs(s / 2);
+    let start_y = 0;
+    let width_of_box = image_width + s;
+    let height_of_box = image_height;
+    console.log(wall_width, wall_height, [start_x, start_y, width_of_box, height_of_box]);
+    return [start_x, start_y, width_of_box, height_of_box];
+  } else {
+    // subtract height
+    let s = image_width / wall_aspect_ratio - image_height;
+    let start_x = 0;
+    let start_y = Math.abs(s / 2);
+    let width_of_box = image_width;
+    let height_of_box = image_height + s;
+    console.log(wall_width, wall_height, [start_x, start_y, width_of_box, height_of_box]);
+    return [start_x, start_y, width_of_box, height_of_box];
+  }
+}
+
+function updateCropper() {
+  let wall_width = parseFloat(document.getElementById('dataWidthCM').value);
+  let wall_height = parseFloat(document.getElementById('dataHeightCM').value);
+  // let image_width = document.getElementById('image').naturalWidth;
+  // let image_height = document.getElementById('image').naturalHeight;
 
 
+  let image_width = parseInt(document.getElementById('image').cropper.canvas.style.width.replace('px', ''))
+  let image_height = parseInt(document.getElementById('image').cropper.canvas.style.height.replace('px', ''))
+  console.log(wall_width, wall_height, image_width, image_height);
+
+  let box = drawBox(wall_width, wall_height, image_width, image_height);
+  cropper.cropper('setCropBoxData', {
+    left: box[0],
+    top: box[1],
+    width: box[2],
+    height: box[3]
+  });
+}
+
+// Initialize the cropper with default values
+let wall_width = parseFloat(document.getElementById('dataWidthCM').value);
+let wall_height = parseFloat(document.getElementById('dataHeightCM').value);
+let image_width = document.getElementById('image').naturalWidth;
+let image_height = document.getElementById('image').naturalHeight;
+
+let box = drawBox(wall_width, wall_height, image_width, image_height);
+let cropper = $('#image').cropper({
+  aspectRatio: box[2] / box[3],
+  crop: function (event) {
+    // console.log(event.detail.x);
+    // console.log(event.detail.y);
+    // console.log(event.detail.width);
+    // console.log(event.detail.height);
+    // console.log(event.detail.rotate);
+    // console.log(event.detail.scaleX);
+    // console.log(event.detail.scaleY);
+  }
 });
+
+// Attach onchange event to input fields
+document.getElementById('dataWidthCM').addEventListener('change', updateCropper);
+document.getElementById('dataHeightCM').addEventListener('change', updateCropper);
